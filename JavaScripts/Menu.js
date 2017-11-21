853dcf9e9854665c9920fc5dbbbbcfc9e3f23776
John Hewitt <jhewitt@actii.org>
2017-11-20 19:13:52 -0500

    Init
    Initial check in

---
+++ JavaScripts/Menu.js
@@ -0,0 +1,35 @@
+function mmLoadMenus() {
+  if (window.mm_menu_0201000806_0) return;
+  window.mm_menu_0201000806_0 = new Menu("root",155,16,"Verdana, Arial, Helvetica, sans-serif",10,"#ffffff","#ffffff","#9e689e","#cc99cc","center","middle",3,0,500,-5,7,true,true,true,0,false,true);
+  mm_menu_0201000806_0.addMenuItem("Weddings","window.open('Wedding.html', '_self');");
+   mm_menu_0201000806_0.fontWeight="bold";
+   mm_menu_0201000806_0.hideOnMouseOut=true;
+   mm_menu_0201000806_0.menuBorder=1;
+   mm_menu_0201000806_0.menuLiteBgColor='#ffffff';
+   mm_menu_0201000806_0.menuBorderBgColor='#ffffff';
+   mm_menu_0201000806_0.bgColor='#ffffff';
+  window.mm_menu_0201000806_1 = new Menu("root",160,16,"Verdana, Arial, Helvetica, sans-serif",10,"#ffffff","#ffffff","#9e689e","#cc99cc","center","middle",3,0,500,-5,7,true,true,true,0,false,true);
+  mm_menu_0201000806_1.addMenuItem("Pre-Marital&nbsp;Coaching","window.open('PreMarital.html', '_self');");
+  mm_menu_0201000806_1.addMenuItem("Wellness&nbsp;Coaching","window.open('WellCoach.html', '_self');");
+  mm_menu_0201000806_1.addMenuItem("TransitionStrategies","window.open('StepCoaching.html', '_self');");
+   mm_menu_0201000806_1.fontWeight="bold";
+   mm_menu_0201000806_1.hideOnMouseOut=true;
+   mm_menu_0201000806_1.menuBorder=1;
+   mm_menu_0201000806_1.menuLiteBgColor='#ffffff';
+   mm_menu_0201000806_1.menuBorderBgColor='#ffffff';
+   mm_menu_0201000806_1.bgColor='#ffffff';
+  window.mm_menu_0201162152_2 = new Menu("root",139,16,"Verdana, Arial, Helvetica, sans-serif",10,"#ffffff","#ffffff","#996699","#cc99cc","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+  mm_menu_0201162152_2.addMenuItem("History","window.open('History.html', '_self');");
+  mm_menu_0201162152_2.addMenuItem("Mission","window.open('Mission.html', '_self');");
+  mm_menu_0201162152_2.addMenuItem("Professional&nbsp;Staff","window.open('ProStaff.html', '_self');");
+   mm_menu_0201162152_2.fontWeight="bold";
+   mm_menu_0201162152_2.hideOnMouseOut=true;
+   mm_menu_0201162152_2.menuBorder=1;
+   mm_menu_0201162152_2.menuLiteBgColor='#ffffff';
+   mm_menu_0201162152_2.menuBorderBgColor='#ffffff';
+   mm_menu_0201162152_2.bgColor='#ffffff';
+
+  mm_menu_0201162152_2.writeMenus();
+} // mmLoadMenus()
+
+//-->
